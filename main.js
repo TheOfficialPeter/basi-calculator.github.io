12b5ccee1e48e6c11d7865b97cc94e6d5cae2ef9
Peter <57006688+TheOfficialPeter@users.noreply.github.com>
2022-08-02 11:34:48 +0000

    added stuff

--- main.js
+++ main.js
@@ -6,30 +6,32 @@ window.onload = function() {
 
 	for (var i = 0; i < buttons.length; i++) {
 		x += 1;
+
 		var currentButton = document.createElement("h1");
 		var input = document.createElement("h1");
-		input.style = "position: absolute; top: 50px; left: 100px;";
+		input.style = "position: absolute; top: 50px; left: 100px; font-family: Arial";
 
+		currentButton.id = buttons[i];
 		currentButton.innerText = buttons[i];
 		currentButton.style = "cursor: pointer; text-align: center; font-family: Arial; font-color: #2e2e2e; background-color: #d2d2d2; border-radius: 10px; width: 100px; height: 50px; position: absolute";
 
 		if (x == 4) {
 			x = 1;
-			y += 1; 
-			currentButton.style.left = (padding*x).toString();
-			currentButton.style.top = (padding*y).toString();
+			y += 1;
+			currentButton.style.left = (padding * x).toString();
+			currentButton.style.top = (padding * y).toString();
 		}
 		else {
-			currentButton.style.left = (padding*x).toString();
-			currentButton.style.top = (padding*y).toString();
+			currentButton.style.left = (padding * x).toString();
+			currentButton.style.top = (padding * y).toString();
+		}
+
+		currentButton.onmousedown = function() {
+			console.log(currentButton.id)
+			input.innerText = currentButton.innerText;
 		}
 
 		document.body.appendChild(currentButton);
 		document.body.appendChild(input);
-
-		currentButton.onclick = function() {
-			console.log(currentButton.innerHTML);
-			input.innerText = currentButton.innerText;
-		}
 	}
 }
